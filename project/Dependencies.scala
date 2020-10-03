a98bc934f03dfb5c2013e373ad1c5a7e4d65a07b
WienerNir <nir.weiner@riskified.com>
2020-10-03 21:17:10 +0300

    adding dependencies

---
+++ project/Dependencies.scala
@@ -0,0 +1,59 @@
+import sbt._
+
+object Dependencies {
+
+  val akkaHttpVersion = "10.1.12"
+  val akkaVersion = "2.6.6"
+  val circeVersion = "0.12.3"
+  val enumeratumCֹirceVersion = "1.5.18"
+  val enumeratumVersion = "1.5.13"
+
+  private val akka: List[ModuleID] = List(
+    "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
+    "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttpVersion,
+    "com.typesafe.akka" %% "akka-actor-typed" % akkaVersion,
+    "com.typesafe.akka" %% "akka-stream" % akkaVersion,
+    "com.typesafe.akka" %% "akka-http-testkit" % akkaHttpVersion % Test,
+    "com.typesafe.akka" %% "akka-actor-testkit-typed" % akkaVersion % Test
+  )
+  private val unitTests: List[ModuleID] = List(
+    "org.scalatest" %% "scalatest" % "3.0.6" % Test,
+    "org.scalamock" %% "scalamock" % "4.1.0" % Test,
+  )
+
+  val enumeratum: List[ModuleID] = List(
+    "com.beachape" %% "enumeratum" % enumeratumVersion,
+    "com.beachape" %% "enumeratum-circe" % enumeratumCֹirceVersion
+  )
+
+  val common: List[ModuleID] = List(
+    "ch.qos.logback" % "logback-classic" % "1.2.3",
+    "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2",
+    "com.github.pureconfig" %% "pureconfig" % "0.14.0",
+    "com.softwaremill.retry" %% "retry" % "0.3.3",
+    "io.circe" %% "circe-derivation" % "0.12.0-M3",
+    "org.typelevel" %% "cats-core" % "2.0.0",
+    "com.softwaremill.quicklens" %% "quicklens" % "1.6.0"
+  )
+
+  val circe: Seq[ModuleID] = Seq(
+    "io.circe" %% "circe-core",
+    "io.circe" %% "circe-generic",
+    "io.circe" %% "circe-parser",
+  ).map(_ % circeVersion)
+
+  val it: Seq[ModuleID] = Seq(
+    "io.monix" %% "monix" % "3.1.0" % Test,
+    "org.testcontainers" % "testcontainers" % "1.12.3" % Test,
+    "com.amazonaws" % "aws-java-sdk-sqs" % "1.11.368" % Test,
+    "org.gnieh" %% "diffson-circe" % "4.0.1" % Test,
+    "org.scalatest" %% "scalatest" % "3.0.6" % Test,
+    "org.scalamock" %% "scalamock" % "4.1.0" % Test
+  )
+
+  val serverItDependencies: List[ModuleID] = unitTests ++ it
+
+  val commonDependencies: Seq[ModuleID] = circe ++ common ++ akka ++ enumeratum
+
+  val serverDependencies: Seq[ModuleID] = commonDependencies ++ unitTests
+}
